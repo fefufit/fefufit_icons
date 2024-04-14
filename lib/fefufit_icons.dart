b367fd941921c3b13073e6d2a8c92648516c724c
rn7cvj <rn7cvj@outlook.com>
2024-04-14 23:18:39 +1000

    init

---
+++ lib/fefufit_icons.dart
@@ -0,0 +1,35 @@
+library;
+
+import 'package:flutter/widgets.dart';
+
+class FFIcons {
+  FFIcons._();
+
+  static const _kFontFam = 'FFIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData info =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData location =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData notif =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData person =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData settings =
+      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrows_left =
+      IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrow_right =
+      IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData edit =
+      IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home =
+      IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData services =
+      IconData(0xe80a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData timetable =
+      IconData(0xe80b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
